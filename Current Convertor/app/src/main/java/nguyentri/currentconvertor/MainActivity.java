de027f3fd4e394168fa91af75a0f3c323b709aaa
nguyentrongty <nguyentriktqd@gmail.com>
2021-08-14 09:57:20 +0700

    update

---
+++ Current Convertor/app/src/main/java/nguyentri/currentconvertor/MainActivity.java
@@ -0,0 +1,29 @@
+package nguyentri.currentconvertor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void convertCurrency(View view) {
+        Log.i("Info", "Convert starting");
+        EditText editText = findViewById(R.id.editText);
+        TextView VNDtextView = findViewById(R.id.VNDtextView);
+        String amountInUSD = editText.getText().toString();
+        double amountInUSDDouble = Double.parseDouble(amountInUSD);
+        double amountInVNDsDoubles = amountInUSDDouble * 23000;
+        String result = String.format("%.2f USD convert to %.2f VND", amountInUSDDouble, amountInVNDsDoubles);
+        VNDtextView.setText(result);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
